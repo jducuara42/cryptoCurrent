910986d3bdef624ae4a82da83090190f98d33a2b
Jeisson Ducuara <jeisson.ducuara@gmail.com>
2022-01-17 20:21:18 -0500

    files in comentarios

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = function(api) {
+  const isTest = api.env('test');
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo', ['@babel/preset-env', {targets: {node: 'current'}}], '@babel/preset-typescript',
+  ],
   };
 };
